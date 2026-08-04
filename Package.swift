eb69906249ec42246d2317b63b7b2e13a2b27ce2
adamz <adam@zethrae.us>
2026-08-03 20:57:35 -0700

    0.6.1

--- Package.swift
+++ Package.swift
@@ -47,7 +47,7 @@ let package = Package(
   dependencies: [
     .package(
       url: "https://github.com/SwiftTUI/swift-tui.git",
-      exact: "0.6.0"
+      exact: "0.6.1"
     ),
     .package(
       url: "https://github.com/swiftlang/swift-docc-plugin.git",
